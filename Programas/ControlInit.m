02802c20d3099e8fff2eb750d07b811036d55a41
Raul <alex_lion345@hotmail.com>
2025-01-17 16:35:53 -0600

    sigo con el control

--- Programas/ControlInit.m
+++ Programas/ControlInit.m
@@ -10,18 +10,23 @@ n=20; %Nodos descontando el fijo
 
 V0=Data(:,1);
 V0(1:20)=Data(1:20,1)-35*ones(20,1);
-Am=A(21:40,1:20);
+Am=A(21:40,:);
 Psi1=[V1 zeros(n,n)];
 Psi2=[zeros(n,n) V2];
 B=[zeros(n,n);eye(n,n)];
 
-% 
-eig_vals = eig(A); % Calcula los valores propios
-is_hurwitz = all(real(eig_vals) < 0); % Verifica si todos tienen parte real negativa
 
-if is_hurwitz
-    disp('La matriz es Hurwitz.');
-else
-    disp('La matriz no es Hurwitz.');
-end
+% eig_vals = eig(A); % Calcula los valores propios
+% is_hurwitz = all(real(eig_vals) < 0); % Verifica si todos tienen parte real negativa
+% 
+% if is_hurwitz
+%     disp('La matriz es Hurwitz.');
+% else
+%     disp('La matriz no es Hurwitz.');
+% end
+
+%% Control de Poznyack
+
+k=-10*ones(n,1);
+
 
